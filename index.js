6bba595b0562feccda00b6c627ec9fea3014f95c
Ryan Breen <rbreen@cimpress.com>
2016-10-03 15:55:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var spawn = require('child_process').spawn;
+var request = require('request');
+var zlib = require('zlib');
+
+var last_change = undefined;
+
+var check = function() {
+
+  request('http://projects.fivethirtyeight.com/2016-election-forecast/', {encoding: null}, function(err, response, body){
+    
+    var handle = function(text) {
+      var match = text.match(/race\.stateData = ([^;]*)/m);
+      var obj = JSON.parse(match[1]);
+      var clinton = obj.sentences.polls.leader + ': ' + obj.sentences.polls.probability;
+
+      if (last_change != clinton) {
+        last_change = clinton;
+        console.log("[%s] %s", new Date(), clinton);
+
+        var display = spawn('osascript', ['-e', 'display notification "' + clinton + '" with title "Polls Changed"']);
+        display.stderr.on('data', (data) => {
+          console.log(`stderr: ${data}`);
+        });
+      }
+    };
+
+    if(response.headers['content-encoding'] == 'gzip'){
+      zlib.gunzip(body, function(err, dezipped) {
+        handle(dezipped.toString());
+      });
+    } else {
+      handle(body);
+    }
+  });
+}
+
+check();
+
+setInterval(check, 60*1000);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Usage
+
+git clone this project
+npm install
+node .
